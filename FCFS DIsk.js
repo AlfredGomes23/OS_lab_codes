09654f091cc3649e09f8b8ef776a869418c27ae8
AlfredGomes23 <alfredgomes23@gmail.com>
2025-03-04 12:17:17 +0600

    FCFS Disk algo

---
+++ FCFS DIsk.js
@@ -0,0 +1,16 @@
+reqSeq = [176, 79, 34, 60, 92, 11, 41, 114];
+head = 50; //printing inputs
+process.stdout.write("FCFS Disk Scheduling Algorithm:\nInputs:\nRequest Sequence:  ");
+reqSeq.forEach(n => process.stdout.write(n + " "));
+console.log("    and the Head: ", head, "\n");
+
+currentPosition = head, seekOp = 0;
+
+reqSeq.forEach(req => {
+    seekOp += Math.abs(currentPosition - req);
+    currentPosition = req;
+});
+console.log("Output:");
+console.log("Total Seek Operations: ", seekOp);
+process.stdout.write("Sequence Order:  ");
+reqSeq.forEach(n => process.stdout.write(n + " "));
