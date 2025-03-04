5b6ae26225dbebc7adcce68c08d0ceda753a1def
AlfredGomes23 <alfredgomes23@gmail.com>
2025-03-04 07:41:29 +0600

    updated rr algo

--- RR.js
+++ RR.js
@@ -15,7 +15,7 @@ input.forEach(process => {
 const timeQuantum = 2;                                 // time quantum for RR algorithm
 let processes = [...input], output = [];                    //copy of input array
 processes.sort((p1, p2) => p1.Arrival - p2.Arrival);        // Sort by Arrival Time
-let readyQueue = [], runningQueue = [], currentTime = 0;    // Initialize Variables
+let readyQueue = [], ganttChart = [], currentTime = 0;    // Initialize Variables
 for (let i = 0; i < processes.length; i++) {                // figuring out the initial
     if (processes[i].Arrival <= currentTime) {              // ready queue processes
         readyQueue.push(processes[i]);
@@ -25,6 +25,7 @@ for (let i = 0; i < processes.length; i++) {                // figuring out the 
 };      // implementing the RR algorithm
 while (readyQueue.length > 0 || processes.length > 0) {
     let currentProcess = readyQueue.shift();        // remove the first ready queue process
+    ganttChart.push(`P${currentProcess.Process}`);        // add  to the gantt chart
     if (currentProcess.Response === undefined){      // response time for first time only
         currentProcess.Response = currentTime - currentProcess.Arrival;//calculate response time
     };    // update the times
@@ -52,8 +53,11 @@ while (readyQueue.length > 0 || processes.length > 0) {
     } else { // if not completed, add it back to the ready queue
         readyQueue.push(currentProcess);
     };
-};      // printing the output
-console.log("Output after Calculation:");
+};      //printing gantt chart
+process.stdout.write("Gantt Chart: | ");
+ganttChart.forEach(p => process.stdout.write(p + " | "));                
+console.log("\n");
+console.log("Output after Calculation:");// printing the output
 output.sort((p1, p2) => p1.Process - p2.Process);       //sorting the output array
 output.forEach(p => {
     console.log(`Process: ${p.Process}, Completion: ${p.Completion}, Turnaround: ${p.Turnaround}, Waiting: ${p.Waiting}, Response: ${p.Response}`);
